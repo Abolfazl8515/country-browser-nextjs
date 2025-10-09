c22de45069e8c0966f4f0571dc31c01b4a7dadfa
Abolfazl <boorbooryabolfazl85@gmail.com>
2025-10-09 16:37:29 +0330

    fixed some minor bugs

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,52 +1,13 @@
 "use client";
-import CardCountry from "@/components/CardCountry";
-import Header from "@/components/Header";
-import Loading from "@/components/ui/Loading";
-import useFilteredCountry from "@/hooks/useFilteredCountry";
-import useGetCountries from "@/hooks/useGetCountries";
-import { useSearchParams } from "next/navigation";
 
-const QUERY = "fields=name,region,flag,capital,id";
+import HomeContent from "@/components/HomeContent";
+import Loading from "@/components/ui/Loading";
+import { Suspense } from "react";
 
 export default function Home() {
-  const searchParams = useSearchParams();
-  const search = searchParams.get("search");
-  const region = searchParams.get("region");
-  const { countries, isLoading } = useGetCountries(QUERY, search);
-  const { filteredCountries, isLoadingFilter } = useFilteredCountry(
-    region ?? ""
-  );
-  const combined = [...(countries || []), ...(filteredCountries || [])];
-
-  if (!isLoading && !isLoadingFilter && combined.length === 0) {
-    return (
-      <div className="w-full space-y-5">
-        <Header />
-        <main className="w-full flex flex-wrap gap-3 justify-center">
-          <p>Nothing found</p>
-        </main>
-      </div>
-    );
-  }
-
   return (
-    <div className="w-full space-y-5">
-      <Header />
-      <main className="w-full flex flex-wrap gap-3 justify-center">
-        {isLoading || isLoadingFilter ? (
-          <Loading />
-        ) : (
-          combined?.map((country) => (
-            <CardCountry
-              key={Math.random()}
-              capital={country.capital}
-              flag={country.flag}
-              name={country.name.official}
-              region={country.region}
-            />
-          ))
-        )}
-      </main>
-    </div>
+    <Suspense fallback={<Loading />}>
+      <HomeContent />
+    </Suspense>
   );
 }
