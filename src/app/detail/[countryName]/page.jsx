9f1237c45e4467e4aede5ed4bd56f677239faaa6
Abolfazl <boorbooryabolfazl85@gmail.com>
2025-10-09 15:31:51 +0330

    detail page

---
+++ src/app/detail/[countryName]/page.jsx
@@ -0,0 +1,56 @@
+"use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Loading from "@/components/ui/Loading";
+import useGetCountries from "@/hooks/useGetCountries";
+import { numberWithCommas } from "@/utils/numberFormatter";
+import { useParams } from "next/navigation";
+
+function DetailPage() {
+  const { countryName } = useParams();
+  const { countries, isLoading } = useGetCountries("", countryName);
+  const country = countries[0];
+  const nativeNames = country?.name?.nativeName;
+  const firstNative = nativeNames ? Object.values(nativeNames)[0] : null;
+
+  return (
+    <main className="w-full h-screen flex justify-center items-center">
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Card className="w-11/12 flex justify-center items-center rounded-xl border border-secondary-400 bg-card text-card-foreground shadow-sm hover:shadow-lg">
+          <CardHeader className="w-full flex items-center gap-4">
+            <div>
+              <span className="w-40 h-40 rounded-full flex items-center justify-center bg-primary-600 text-secondary-50 text-6xl">
+                {country?.flag}
+              </span>
+            </div>
+            <div>
+              <CardTitle className="text-4xl font-black">
+                {country.name?.official}
+              </CardTitle>
+            </div>
+          </CardHeader>
+          <CardContent className="w-full space-y-3">
+            <p className="w-11/12 text-secondary-50 bg-primary-600 p-3 rounded-4xl">
+              Native name: {firstNative?.official || "N/A"}
+            </p>
+            <p className="w-11/12 text-secondary-50 bg-primary-600 p-3 rounded-4xl">
+              capital: {country.capital}
+            </p>
+            <p className="w-11/12 text-secondary-50 bg-primary-600 p-3 rounded-4xl">
+              region: {country.region}
+            </p>
+            <p className="w-11/12 text-secondary-50 bg-primary-600 p-3 rounded-4xl">
+              sub region: {country.subregion}
+            </p>
+            <p className="w-11/12 text-secondary-50 bg-primary-600 p-3 rounded-4xl">
+              population: {numberWithCommas(country.population)}
+            </p>
+          </CardContent>
+        </Card>
+      )}
+    </main>
+  );
+}
+
+export default DetailPage;
--- src/components/CardCountry.jsx
+++ src/components/CardCountry.jsx
@@ -7,25 +7,32 @@ import {
   CardContent,
   CardFooter,
 } from "@/components/ui/card";
+import Link from "next/link";
 
 function CardCountry({ flag, name, region, capital }) {
   return (
-    <Card className="w-[280px] rounded-xl border border-secondary-400 bg-card text-card-foreground shadow-sm hover:shadow-lg hover:-translate-y-1 transition-all duration-300">
-      <CardHeader className="flex items-center gap-4">
-        <div>
-          <span className="w-20 h-20 rounded-full flex items-center justify-center bg-primary-600 text-secondary-50 text-2xl">
-            {flag}
-          </span>
-        </div>
-        <div>
-          <CardTitle className="text-lg">{name}</CardTitle>
-        </div>
-      </CardHeader>
-      <CardContent className="space-y-3">
-        <p className="text-secondary-50 bg-primary-600 p-3 rounded-4xl">capital: {capital}</p>
-        <p className="text-secondary-50 bg-primary-600 p-3 rounded-4xl">region: {region}</p>
-      </CardContent>
-    </Card>
+    <Link href={`/detail/${name}`}>
+      <Card className="w-[280px] cursor-pointer rounded-xl border border-secondary-400 bg-card text-card-foreground shadow-sm hover:shadow-lg hover:-translate-y-1 transition-all duration-300">
+        <CardHeader className="flex items-center gap-4">
+          <div>
+            <span className="w-20 h-20 rounded-full flex items-center justify-center bg-primary-600 text-secondary-50 text-2xl">
+              {flag}
+            </span>
+          </div>
+          <div>
+            <CardTitle className="text-lg">{name}</CardTitle>
+          </div>
+        </CardHeader>
+        <CardContent className="space-y-3">
+          <p className="text-secondary-50 bg-primary-600 p-3 rounded-4xl">
+            capital: {capital}
+          </p>
+          <p className="text-secondary-50 bg-primary-600 p-3 rounded-4xl">
+            region: {region}
+          </p>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
 
